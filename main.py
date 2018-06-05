6c616d91270b147a1febf9c9bb633e82b8837c31
avielkar <avraham.elkar@gmail.com>
2018-06-05 15:18:20 +0300

    Make the main more pretty.

--- main.py
+++ main.py
@@ -2,17 +2,27 @@
 from psychopy import visual, event, core
 
 # create a window to draw in
-myWin = visual.Window((600, 600), allowGUI=False,
-                      bitsMode=None, units='norm', winType='pyglet')
+myWin = visual.Window((600, 600),
+                      allowGUI=False,
+                      bitsMode=None,
+                      units='norm',
+                      winType='pyglet')
 
 # INITIALISE SOME STIMULI
-dotPatch = visual.DotStim(myWin, color=(1.0, 1.0, 1.0), dir=270,
-                          nDots=500, fieldShape='circle', fieldPos=(0.0, 0.0), fieldSize=1,
+dotPatch = visual.DotStim(win=myWin,
+                          color=(1.0, 1.0, 1.0),
+                          dir=270,
+                          nDots=500, fieldShape='circle',
+                          fieldPos=(0.0, 0.0),
+                          fieldSize=1,
                           dotLife=5,  # number of frames for each dot to be drawn
                           signalDots='same',  # are the signal dots the 'same' on each frame? (see Scase et al)
                           noiseDots='direction',  # do the noise dots follow random- 'walk', 'direction', or 'position'
-                          speed=0.01, coherence=0.9)
-message = visual.TextStim(myWin, text='Hit Q to quit',
+                          speed=0.01,
+                          coherence=0.9)
+
+message = visual.TextStim(win=myWin,
+                          text='Hit Q to quit',
                           pos=(0, -0.5))
 trialClock = core.Clock()
 while True:  # forever
