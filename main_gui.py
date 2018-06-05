322dbda88341148ac654fd87f2d13c6632d12669
avielkar <avraham.elkar@gmail.com>
2018-06-05 17:05:41 +0300

    Addeed gui parameters for the RDK experiment.

--- main_gui.py
+++ main_gui.py
@@ -1,9 +1,21 @@
 from psychopy import gui
 
-info = {'Observer':'jwp',
-        'GratingOri':45,
+info = {
         'ExpVersion': 1.1,
+        'Observer': 'jwp',
+        'GratingOri': 45,
         'Group': ['Test', 'Control'],
+        'DotsColor': [1.0, 1.0, 1.0],
+        'Direction': 270,
+        'NumberOfDots': 500,
+        'FieldShape': 'circle',
+        'FieldPosition': [0.0, 0.0],
+        'FieldSize': 1,
+        'DotLife': 5,
+        'SignalDots': 'same',
+        'NoiseDots': 'direction',
+        'Speed': 0.01,
+        'Coherence': 0.9
         }
 dictDlg = gui.DlgFromDict(dictionary=info,
                           title="RDK",
@@ -13,8 +25,6 @@ if dictDlg.OK:
     print(info)
 else:
     print('User Cancelled')
-
-dictDlg.show()
 
 keyWait = input('Press key to quit')
 dictDlg.Close()
