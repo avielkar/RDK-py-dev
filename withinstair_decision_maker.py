b93bae040c69376fe2a77cea0eaf79b8a7cad773
avielkar <avraham.elkar@gmail.com>
2018-06-15 01:04:13 +0300

    Add logics for the create_within_stair_vector function and linked the trial maker to it.

--- withinstair_decision_maker.py
+++ withinstair_decision_maker.py
@@ -1,18 +1,15 @@
-
 class WithinStairDecisionMaker:
-    backword_rightword_probability = None  # type: Integer
-    backword_error_probability = None  # type: Integer
-    attribute = None  # type: Any
-
     def __init__(self):
+        self.backword_rightword_probability = None  # type: Integer
+        self.backword_error_probability = None  # type: Integer
+        self.param_attributes = None  # type: Anys
         pass
 
-
     def set_attributes(self,
-                       attribute,
+                       param_attributes,
                        backword_error_probability,
                        backword_rightword_probability):
-        self.attribute = attribute
+        self.param_attributes = param_attributes
         self.backword_error_probability = backword_error_probability
         self.backword_rightword_probability = backword_rightword_probability
         pass
@@ -21,10 +18,13 @@ class WithinStairDecisionMaker:
         pass
 
     def create_within_stair_vector(self):
-        within_stair_low_val = attribute
-        within_stair_high_value=0
-        within_stair_jumping = 0
+        within_stair_low_val = self.param_attributes['min_value']
+        within_stair_high_value = self.param_attributes['max_value']
+        within_stair_jumping = self.param_attributes['jumping']
+        within_vector = range(within_stair_low_val,
+                              within_stair_high_value,
+                              within_stair_jumping)
+        return within_vector
 
-    def current_trial(self , previous_decision_correction):
-
+    def current_trial(self, previous_decision_correction):
         pass
