50355e9f85b2e2dec0d78febe3aeab8e01449504
avielkar <avraham.elkar@gmail.com>
2018-06-14 22:38:08 +0300

    Add trial maker plus change the behavior of the within stair decision maker to only gets its attribute.

---
+++ trialmaker.py
@@ -0,0 +1,5 @@
+
+class TrialMaker:
+    def __init__(self):
+        pass
+
--- withinstair_decision_maker.py
+++ withinstair_decision_maker.py
@@ -1,10 +1,20 @@
 
 class WithinStairDecisionMaker:
+    backword_rightword_probability = None  # type: Integer
+    backword_error_probability = None  # type: Integer
+    attribute = None  # type: Any
+
     def __init__(self):
         pass
 
 
-    def SetAttributes(self):
+    def SetAttributes(self,
+                      attribute,
+                      backword_error_probability,
+                      backword_rightword_probability):
+        self.attribute = attribute
+        self.backword_error_probability = backword_error_probability
+        self.backword_rightword_probability = backword_rightword_probability
         pass
 
     def ResetDecision(self):
