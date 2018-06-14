f71210dddd3d276cf2e21c08e266773d84b09321
avielkar <avraham.elkar@gmail.com>
2018-06-14 22:55:59 +0300

    Create the TrialMaker class with functions definition without logic.

--- trialmaker.py
+++ trialmaker.py
@@ -1,5 +1,20 @@
-
 class TrialMaker:
     def __init__(self):
+        self._attributes = None  # type: Dict[Any, Any]
+        self._numOfTrials = None  # type: Integer
+        self._numOfRepetitions = None  # type: Integer
+        self.trial_number = 0
         pass
 
+    def current_trial(self):
+        pass
+
+    def load_new_data(self,
+                      attributes,
+                      num_of_repetitions,
+                      num_of_trials):
+        self._attributes = attributes
+        self._numOfTrials = num_of_trials
+        self._numOfRepetitions = num_of_repetitions
+        self.trial_number = 0
+        pass
--- withinstair_decision_maker.py
+++ withinstair_decision_maker.py
@@ -20,5 +20,5 @@ class WithinStairDecisionMaker:
     def ResetDecision(self):
         pass
 
-    def NextDecision(self):
+    def CurrentTrial(self):
         pass
