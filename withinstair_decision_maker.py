438c5b9330d42230e413e9b2fd1d463f14830d1c
avielkar <avraham.elkar@gmail.com>
2018-06-14 22:13:35 +0300

    Add the withinstair decision class with define functions - no logic yet.

---
+++ withinstair_decision_maker.py
@@ -0,0 +1,14 @@
+
+class WithinStairDecisionMaker:
+    def __init__(self):
+        pass
+
+
+    def SetAttributes(self):
+        pass
+
+    def ResetDecision(self):
+        pass
+
+    def NextDecision(self):
+        pass
