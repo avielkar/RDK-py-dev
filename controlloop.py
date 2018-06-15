99023a37d75d91314c64ce82fb419c2c6f1c52b9
avielkar <avraham.elkar@gmail.com>
2018-06-15 20:29:17 +0300

    Print 'no response' if there is no response in the given response time.

--- controlloop.py
+++ controlloop.py
@@ -45,3 +45,5 @@ class ControlLoop:
                                        keyList=['left', 'right'])
         if keys:
             print ('pressed {key}'.format(key=keys[0]))
+        else:
+            print 'no response'
