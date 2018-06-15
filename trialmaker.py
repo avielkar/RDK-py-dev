172653fb9366872160b81cc2e17e49ef90e5f10e
avielkar <avraham.elkar@gmail.com>
2018-06-15 13:10:52 +0300

    Make the function of the maker to return the dictionary value (include the parameter name).

--- trialmaker.py
+++ trialmaker.py
@@ -25,7 +25,7 @@ class TrialMaker:
         if experiment_type == 'withinstair':
             self.within_stair_decision_maker = WithinStairDecisionMaker()
             self.within_stair_decision_maker.set_attributes(
-                (filter(lambda x: self._attributes[x]['param_type'] == 'withinstair', self._attributes.keys()))[0],
+                self._attributes[(filter(lambda x: self._attributes[x]['param_type'] == 'withinstair', self._attributes.keys()))[0]],
                 backword_error_probability=0.9,
                 backword_rightword_probability=0.1)
         elif experiment_type == 'varying':
