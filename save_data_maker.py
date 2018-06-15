7c6ad47803d4c005bf2217a2b2230978f43bcb92
avielkar <avraham.elkar@gmail.com>
2018-06-15 23:13:03 +0300

    Add save_data_maker class with functions with no logic.

---
+++ save_data_maker.py
@@ -0,0 +1,9 @@
+class SaveDataMaker:
+    def __init__(self):
+        pass
+
+    def create_new_data_file(self, file_path):
+        pass
+
+    def save_trial_to_file(self, trial_data):
+        pass
