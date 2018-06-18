3080ff7a73e6d35994651f41436691de941b7cb9
avielkar <avraham.elkar@gmail.com>
2018-06-18 21:21:01 +0300

    Added analyze_response function to decide if the response was the correct direction of the movement.

---
+++ response_analyzer.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+class ResponseAnalyzer:
+    def __init__(self):
+        pass
+
+    def analyze_response(self , trial_data,response):
+        current_direction = float(trial_data['Direction'])
+        if 180 > current_direction > 0 and response == 'right':
+            return True
+        elif 180 > current_direction > 0 and response == 'left':
+            return False
+        elif 180 > current_direction and response == 'right':
+            return False
+        elif 180 < current_direction and response == 'left':
+            return True
+
+        return False
