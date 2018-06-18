269345e51bcd1fe102ad6a487ee5ecb1186b561e
avielkar <avraham.elkar@gmail.com>
2018-06-18 19:18:46 +0300

    Replace the '\' in the directory instead of the file name.

--- save_data_maker.py
+++ save_data_maker.py
@@ -5,13 +5,13 @@ import datetime
 class SaveDataMaker:
 
     def __init__(self):
-        self.directory_path = 'D:\RDK-SavedData'
+        self.directory_path = 'D:\RDK-SavedData\\'
         self.current_saved_file_name = ''
         self.current_saved_file = None  # type: BinaryIO
         pass
 
     def create_new_data_file(self):
-        self.current_saved_file_name = '\{date:%Y-%m-%d %H_%M_%S}.txt' \
+        self.current_saved_file_name = '{date:%Y-%m-%d %H_%M_%S}.txt' \
             .format(date=datetime.datetime.now())
         self.current_saved_file = open(self.directory_path + self.current_saved_file_name, 'wb', buffering=20)
         pass
