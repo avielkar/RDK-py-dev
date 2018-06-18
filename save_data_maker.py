abf73fbe32d164c54cffac1b50b6aff7b6c28689
avielkar <avraham.elkar@gmail.com>
2018-06-18 19:18:00 +0300

    Add buffering to the file writer in the saved data maker.

--- save_data_maker.py
+++ save_data_maker.py
@@ -13,7 +13,7 @@ class SaveDataMaker:
     def create_new_data_file(self):
         self.current_saved_file_name = '\{date:%Y-%m-%d %H_%M_%S}.txt' \
             .format(date=datetime.datetime.now())
-        self.current_saved_file = open(self.directory_path + self.current_saved_file_name, 'wb')
+        self.current_saved_file = open(self.directory_path + self.current_saved_file_name, 'wb', buffering=20)
         pass
 
     def save_trial_data_to_file(self, trial_data):
