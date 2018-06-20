5817b8c16fafaebd39ad306c8d0ebc703b477c55
avielkar <avraham.elkar@gmail.com>
2018-06-20 14:29:24 +0300

    If first trial for right or for left make the init first trials for left and right as false for not advance in the difficulty at the beginning.

--- response_analyzer.py
+++ response_analyzer.py
@@ -2,16 +2,22 @@
 
 class ResponseAnalyzer:
     def __init__(self):
-        self._first_trial = True
+        self._first_trial_left = True
+        self._first_trial_right = True
         pass
 
     def analyze_response(self, trial_data, response):
+        current_direction = float(trial_data['Direction'])
+
         # if it is the firat trial there is no past to look for.....
-        if self._first_trial:
-            self._first_trial = False
+        if self._first_trial_left and 360 > current_direction > 180:
+            self._first_trial_left = False
             return False
 
-        current_direction = float(trial_data['Direction'])
+        if self._first_trial_left and 180 > current_direction > 0:
+            self._first_trial_left = False
+            return False
+
         if 180 > current_direction > 0 and response == 'right':
             return True
         elif 180 > current_direction > 0 and response == 'left':
@@ -24,5 +30,4 @@ class ResponseAnalyzer:
         return False
 
     def reset_analyzer(self):
-        self._first_trial = True
-
+        self._first_trial_left = True
