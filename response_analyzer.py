fc6aff0e5c09dc0f21de7e1fe13cb4d05ada82c6
avielkar <avraham.elkar@gmail.com>
2018-06-20 15:43:55 +0300

    Delete the unnecessary logic for the response analyzer.

--- response_analyzer.py
+++ response_analyzer.py
@@ -2,22 +2,11 @@
 
 class ResponseAnalyzer:
     def __init__(self):
-        self._first_trial_left = True
-        self._first_trial_right = True
         pass
 
     def analyze_response(self, trial_data):
         current_direction = float(trial_data['Direction'])
         response = trial_data['Response']
-
-        # if it is the firat trial there is no past to look for.....
-        if self._first_trial_left and 360 > current_direction > 180:
-            self._first_trial_left = False
-            return False
-
-        if self._first_trial_left and 180 > current_direction > 0:
-            self._first_trial_left = False
-            return False
 
         if 180 > current_direction > 0 and response == 'right':
             return True
