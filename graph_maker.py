c6108063eab3f853343608786805df78d6bf81f1
avielkar <avraham.elkar@gmail.com>
2018-06-22 08:56:41 +0300

    Added the scala_values for the graph_maker from the control loop o the init_graph.

--- graph_maker.py
+++ graph_maker.py
@@ -3,10 +3,13 @@ import numpy as np
 
 
 class GraphMaker:
+
     def __init__(self):
+        self._scala_values = None  # type: object
         pass
 
-    def init_graph(self):
+    def init_graph(self, scala_values):
+        self._scala_values = scala_values
         pass
 
     def update_graph(self, trial_data):
