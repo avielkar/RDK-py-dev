121eee6e6980dfbcf1317b3e4671e687dcdd4435
avielkar <avraham.elkar@gmail.com>
2018-06-29 15:00:50 +0300

    Make the SimpleWithinStairDecisionMaker and ComplexWithinStairDecisionMaker inherit from WithinStairDecisionMaker

--- withinstair_decision_maker.py
+++ withinstair_decision_maker.py
@@ -2,20 +2,25 @@ import numpy
 
 
 class WithinStairDecisionMaker:
-
     def __init__(self):
         self.backword_rightword_probability = None  # type: Integer
         self.backword_error_probability = None  # type: Integer
         self.param_attributes = None  # type: Dict[Any, Any]
         self.static_parameters_attributes = None  # type: List[Any]
         self.within_stair_vector = None  # type: list
+        self.within_stair_attribute = None  # type: Any
+        self.current_trial_attributes = None  # Dict[string, Any]
+        pass
+
+
+class ComplexWithinStairDecisionMaker(WithinStairDecisionMaker):
+
+    def __init__(self):
         self.within_stair_negative_vector_index = 0
         self.within_stair_positive_vector_index = 0
-        self.within_stair_attribute = None  # type: Any
         self.last_trial_direction = None  # type: 'left' , 'right' or 'none'
         self.last_trial_correction_right = None  # type: bool
         self.last_trial_correction_left = None  # type: bool
-        self.current_trial_attributes = None  # Dict[string, Any]
         pass
 
     def set_attributes(self,
@@ -43,7 +48,8 @@ class WithinStairDecisionMaker:
     def create_within_stair_vector(self):
 
         self.within_stair_attribute = self.param_attributes \
-            [list(filter(lambda x: self.param_attributes[x]['paramtype'] == 'withinstair', self.param_attributes.keys()))[
+            [list(
+                filter(lambda x: self.param_attributes[x]['paramtype'] == 'withinstair', self.param_attributes.keys()))[
                 0]]
         self.static_parameters_attributes = (
             list(filter(lambda attributes: attributes['paramtype'] == 'static', self.param_attributes.values())))
@@ -90,7 +96,7 @@ class WithinStairDecisionMaker:
         for param_attributes in self.static_parameters_attributes:
             current_trial_data[param_attributes['param_name']] = param_attributes['value']
         current_trial_data[self.within_stair_attribute['param_name']] = current_withinstair_value
-        current_trial_data['Direction'] = current_trial_data['Direction'] if right_trial\
+        current_trial_data['Direction'] = current_trial_data['Direction'] if right_trial \
             else -current_trial_data['Direction']
         self.current_trial_attributes = current_trial_data
 
@@ -112,5 +118,3 @@ class SimpleWithinstairDecisionMaker:
         self.last_trial_correction_left = None  # type: bool
         self.current_trial_attributes = None  # Dict[string, Any]
         pass
-
-
