fff604c6c6df3a64be88d514cef63ef3731ea2be
avielkar <avraham.elkar@gmail.com>
2018-07-06 14:11:34 +0300

    Delete unnecessary class.

--- graph_maker_process.py
+++
@@ -1,7 +0,0 @@
-from graph_maker import GraphMaker
-
-
-class GraphMakerProcess:
-    def __init__(self , graph_maker_command_queue):
-        self.graph_maker = GraphMaker()
-        self.graph_maker.listening_function_thread(graph_maker_command_queue)
