40e1cf3d981d87c1a32f527f32b2f42a3c5ad7ea
avielkar <avraham.elkar@gmail.com>
2018-07-06 21:10:52 +0300

    Make the logic of the write_function works and save the data good.

--- protocolwriter.py
+++ protocolwriter.py
@@ -1,13 +1,22 @@
 import pandas as pd
 
+
 class ProtocolWriter:
     def init(self):
         pass
 
-    def write_file(self , path , data):
+    def write_file(self, path, data):
         excel_writer = pd.ExcelWriter('D:\RDK-protocols\coherence123.xlsx')
-        protocol_data_frame = pd.DataFrame.from_dict(data)
-        protocol_data_frame.to_excel(excel_writer)
-        excel_writer.save()
-        pass
 
+        excel_data = []
+        for attribute_name in data['Coherence'].keys():
+            column_data = [data[param_name][attribute_name] for param_name in data.keys()]
+            columns_name = attribute_name
+            excel_data.append((columns_name, column_data))
+
+        df = pd.DataFrame.from_items(excel_data)
+        df.to_excel(excel_writer,
+                    index=False)
+        excel_writer.save()
+
+        pass
