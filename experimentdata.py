9e6e1679648ffea7a47a0421db0423e06a6c3b1b
avielkar <avraham.elkar@gmail.com>
2018-07-08 21:07:15 +0300

    Fix spelling error.

--- experimentdata.py
+++ experimentdata.py
@@ -12,11 +12,11 @@ class ExperimentData:
                  backward_error_probability=None,
                  forward_rightward_probability=None,
                  enable_confidence_choice=None,
-                 draw_fixxation_point=None):
+                 draw_fixation_point=None):
         self.num_of_repetitions = num_of_repetitions
         self.num_of_trials = num_of_trials
         self.backward_error_probability = backward_error_probability
         self.forward_rightward_probability = forward_rightward_probability
         self.enable_confidence_choice = enable_confidence_choice
-        self.draw_fixation_point=draw_fixxation_point
+        self.draw_fixation_point=draw_fixation_point
         pass
